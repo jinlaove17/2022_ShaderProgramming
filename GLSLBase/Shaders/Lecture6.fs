2fec81aa4767cb1546db7d26f9182ef15ea20af7
JongWoo <jinlaove17@naver.com>
2022-06-08 13:34:17 +0900

    [코드 추가] 13주 1차시(Lecture 6_2) 실습 : 텍스처좌표 꼬아보기

--- GLSLBase/Shaders/Lecture6.fs
+++ GLSLBase/Shaders/Lecture6.fs
@@ -14,7 +14,55 @@ vec4 Flip()
 	return vNewColor;
 }
 
+vec4 Practice_1()
+{
+	vec2 vNewTexCoord = vec2(v_vTexCoord.x, abs(2.0f * (v_vTexCoord.y - 0.5f)));
+	vec4 vNewColor = texture(u_sTexSampler, vNewTexCoord);
+	
+	return vNewColor;
+}
+
+vec4 Practice_2()
+{
+	vec2 vNewTexCoord = vec2(3.0f * v_vTexCoord.x, v_vTexCoord.y / 3.0f + floor(3.0f * v_vTexCoord.x) / 3.0f);
+	vec4 vNewColor = texture(u_sTexSampler, vNewTexCoord);
+	
+	return vNewColor;
+}
+
+vec4 Practice_3()
+{
+	vec2 vNewTexCoord = vec2(3.0f * v_vTexCoord.x, v_vTexCoord.y / 3.0f + floor(3.0f * (1.0f - v_vTexCoord.x)) / 3.0f);
+	vec4 vNewColor = texture(u_sTexSampler, vNewTexCoord);
+
+	return vNewColor;
+}
+
+vec4 Practice_4()
+{
+	vec2 vNewTexCoord = vec2(v_vTexCoord.x, fract(3.0f * v_vTexCoord.y) / 3.0f + (2.0f - floor(3.0f * v_vTexCoord.y)) / 3.0f);
+	vec4 vNewColor = texture(u_sTexSampler, vNewTexCoord);
+
+return vNewColor;
+}
+
+vec4 Practice_5()
+{
+	vec2 vNewTexCoord = vec2(fract(2.0f * v_vTexCoord.x) + 0.5f * floor(2.0f * v_vTexCoord.y), fract(2.0f * v_vTexCoord.y));
+	vec4 vNewColor = texture(u_sTexSampler, vNewTexCoord);
+	
+	return vNewColor;
+}
+
+vec4 Practice_6()
+{
+	vec2 vNewTexCoord = vec2(fract(2.0f * v_vTexCoord.x), fract(2.0f * v_vTexCoord.y) + 0.5f * floor(2.0f * v_vTexCoord.x));
+	vec4 vNewColor = texture(u_sTexSampler, vNewTexCoord);
+	
+	return vNewColor;
+}
+
 void main()
 {
-	FragColor = Flip();
+	FragColor = Practice_6();
 }
